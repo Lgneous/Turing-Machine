b4f0a1d07f26a0955166680d2e728c79910f906c
Julien Chedal-Anglay <jchedal-@T420.home>
2019-01-19 08:06:58 +0100

    Simplified types

--- src/syntax.ml
+++ src/syntax.ml
@@ -1,41 +1,12 @@
 open Batteries
 
-type ast =
-  | Assoc of (string * ast) list
-  | List of ast list
-  | String of string
+type core =
+  | Name of string
+  | Alphabet of string list
+  | Blank of string
+  | States of string list
+  | Initial of string
+  | Finals of string list
+  | Transitions of (string * (Transition.t list)) list
 
-let is_assoc = function
-  | Assoc _ -> true
-  | _ -> false
-
-let is_list = function
-  | List _ -> true
-  | _ -> false
-
-let for_all_list f = function
-  | List xs -> List.for_all f xs
-  | _ -> false
-
-let is_string = function
-  | String _ -> true
-  | _ -> false
-
-let is_char = function
-  | String s when String.length s = 1 -> true
-  | _ -> false
-
-let assoc_uniq x ast =
-  let rec _assoc_uniq = function
-    | [] -> Result.Bad (x ^ " not found.")
-    | (k, v) :: tl when k = x && not @@ List.mem_assoc x tl -> Result.Ok v
-    | (k, v) :: tl when k = x -> Result.Bad ("Duplicates entries for " ^ x ^ ".")
-    | _ :: tl -> _assoc_uniq tl
-  in
-  match ast with
-  | Assoc xs -> _assoc_uniq xs
-  | _ -> Result.Bad "Invalid type: should be an object."
-
-let filter f err_msg = function
-  | Result.Ok x -> if f x then Result.Ok x else Result.Bad err_msg
-  | err -> err
+type t = core list
--- src/syntax.mli
+++ src/syntax.mli
@@ -1,20 +1,10 @@
-open Batteries
+type core =
+  | Name of string
+  | Alphabet of string list
+  | Blank of string
+  | States of string list
+  | Initial of string
+  | Finals of string list
+  | Transitions of (string * (Transition.t list)) list
 
-type ast =
-  | Assoc of (string * ast) list
-  | List of ast list
-  | String of string
-
-val is_assoc : ast -> bool
-
-val is_list : ast -> bool
-
-val for_all_list : (ast -> bool) -> ast -> bool
-
-val is_string : ast -> bool
-
-val is_char : ast -> bool
-
-val assoc_uniq : string -> ast -> (ast, string) Result.t
-
-val filter : (ast -> bool) -> string -> (ast, string) Result.t -> (ast, string) Result.t
+type t = core list
