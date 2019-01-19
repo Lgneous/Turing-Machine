5939f84ee47526f27130833658bf3c4db7287122
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-01-19 08:06:58 +0100

    Simplified types

--- src/machine.ml
+++ src/machine.ml
@@ -1,19 +1,3 @@
 open Batteries
 
-type t = {
-    name : string;
-    alphabet : char list;
-    blank : char;
-    states : string list;       (* Maybe use a string set *)
-    initial : string;
-    finals : string list;
-    transitions : Transitions.t list
-  }
-
-let of_ast ast =
-  let name = Syntax.filter Syntax.is_string "Invalid type: name value should be a string." @@ Syntax.assoc_uniq "name" ast in
-  let alphabet = Syntax.filter (Syntax.for_all_list Syntax.is_char) "Invalid type: alphabet value should be a char list." @@ Syntax.assoc_uniq "alphabet" ast in
-  let blank = Syntax.filter Syntax.is_char "Invalid type: blank value should be a char." @@ Syntax.assoc_uniq "blank" ast in
-  let states = Syntax.filter (Syntax.for_all_list Syntax.is_string) "Invalid type: states value should be a string list." @@ Syntax.assoc_uniq "states" ast in
-  let initial = Syntax.filter Syntax.is_string "Invalid type: initial value should be a string." @@ Syntax.assoc_uniq "initial" ast in
-  print_bool @@ List.for_all Result.is_ok [name;alphabet;blank;states;initial]
+type t = Syntax.t
