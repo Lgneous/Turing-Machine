6c15c0268ae1301ba085992129402a59fda0c9d8
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-01-24 11:02:07 +0100

    Added proper usage function, changed displaying function's name

--- src/turing.ml
+++ src/turing.ml
@@ -12,16 +12,18 @@ let usage () =
                                    "optional arguments:";
                                    "  -h, --help                show this help message and exit";
                                  ]
-let test = function
+let display_or_err = function
   | Result.Ok d -> Print.machine_desc d
   | Result.Bad e -> print_endline e
 
 let _ =
-  usage ();
-  let f = open_in Sys.argv.(1) in
-  let lexbuf = Lexing.from_channel f in
-  try
-    test @@ Machine.sanitize @@ Machine.of_ast @@ Parser.prog Lexer.read lexbuf
-  with
-    _ -> let pos = lexbuf.lex_curr_p in
-         ignore @@ Printf.printf "line %d, col %d\n" pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+  if Array.length Sys.argv <> 2
+  then usage ()
+  else
+    let f = open_in Sys.argv.(1) in
+    let lexbuf = Lexing.from_channel f in
+    try
+      display_or_err @@ Machine.sanitize @@ Machine.of_ast @@ Parser.prog Lexer.read lexbuf
+    with
+      _ -> let pos = lexbuf.lex_curr_p in
+           ignore @@ Printf.printf "line %d, col %d\n" pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
