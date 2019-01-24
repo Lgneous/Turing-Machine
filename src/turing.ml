478894cbd28f9193aae745d569ca9c9e3baa0ef5
Julien Chedal-Anglay <jchedal-@T420.home>
2019-01-24 02:00:36 +0100

    Added usage function

--- src/turing.ml
+++ src/turing.ml
@@ -1,10 +1,23 @@
 open Batteries
 
+let usage () =
+  print_endline @@ String.concat "\n" [
+                                   "usage: ./turing.byte [-h] jsonfile input";
+                                   "";
+                                   "positional arguments:";
+                                   "  jsonfile                  json description of the machine";
+                                   "";
+                                   "  input                     input of the machine";
+                                   "";
+                                   "optional arguments:";
+                                   "  -h, --help                show this help message and exit";
+                                 ]
 let test = function
   | Result.Ok d -> Print.machine_desc d
   | Result.Bad e -> print_endline e
 
 let _ =
+  usage ();
   let f = open_in Sys.argv.(1) in
   let lexbuf = Lexing.from_channel f in
   try
